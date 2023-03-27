0d71ea1b85c8a7f580d9e088117bc8a9fda9329d
mhuang7100 <mhuang00@ladueschools.net>
2023-03-27 14:43:59 +0000

    Finished?

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class Main{
     public static void main(String[] args){
         System.out.println("What is the equation?");
@@ -20,8 +21,9 @@ public class Main{
     
     // gets player input (String)
     public static String getInput(){
-        Scanner input = new Scanner(System.in);
-        return input.nextLine();
+        try (Scanner input = new Scanner(System.in)) {
+            return input.nextLine();
+        }
     }
 
     public static double solveExpression(String expression){
@@ -30,25 +32,82 @@ public class Main{
         String endExpression = "";
         if (multOperations(expression)){
             
-            if (search(expression, "(")){
-                start = find(expression, "(").get(0);
-                end = find(expression, ")").get(0);
-                return solveExpression(expression.substring(0, start) + solveExpression(expression.substring(start, end)) + expression.substring(end, expression.length()));
-            } else if (search(expression, "^")){
+            if (search(expression, "(") > 0){
+                // if useless parentheses surround the expression, delete them
+                if (expression.substring(0, 1).equals("(") && expression.substring(expression.length() - 1, expression.length()).equals(")")){
+                    expression = expression.substring(1, expression.length() - 1);
+                    return solveExpression(expression);
+                }
+                start = find(expression, "(").get(find(expression, "(").size() - 1);
+                end = find(expression, ")").get(0) + 1;
+                
+                if (!expression.substring(0, start).equals("")){
+                    endExpression += expression.substring(0, start);
+                }
+                endExpression += solveExpression(expression.substring(start, end));
+                if (!expression.substring(end, expression.length()).equals("")){
+                    endExpression += expression.substring(end, expression.length());
+                }
+                System.out.println(endExpression); //ts
+                return solveExpression(endExpression);
+            } else if (search(expression, "^") > 0){
                 start = findStart(expression, "^");
                 end = findEnd(expression, "^");
                 if (!expression.substring(0, start).equals("")){
                     endExpression += expression.substring(0, start);
                 }
-                System.out.println(start + " " + end); // troubleshooting
                 endExpression += solveExpression(expression.substring(start, end));
-                if (!expression.substring(end, expression.length() - 1).equals("")){
-                    endExpression += expression.substring(end, expression.length() - 1);
+                if (!expression.substring(end, expression.length()).equals("")){
+                    endExpression += expression.substring(end, expression.length());
                 }
-                
                 return solveExpression(endExpression);
-            
-            } else return 0.0; // PLACEHOLDER - FINISH THIS
+            } else if (search(expression, "*") > 0){
+                start = findStart(expression, "*");
+                end = findEnd(expression, "*");
+
+                if (!expression.substring(0, start).equals("")){
+                    endExpression += expression.substring(0, start);
+                }
+                endExpression += solveExpression(expression.substring(start, end));
+                if (!expression.substring(end, expression.length()).equals("")){
+                    endExpression += expression.substring(end, expression.length());
+                }
+                return solveExpression(endExpression);
+            } else if (search(expression, "/") > 0){
+                start = findStart(expression, "/");
+                end = findEnd(expression, "/");
+                if (!expression.substring(0, start).equals("")){
+                    endExpression += expression.substring(0, start);
+                }
+                endExpression += solveExpression(expression.substring(start, end));
+                if (!expression.substring(end, expression.length()).equals("")){
+                    endExpression += expression.substring(end, expression.length());
+                }
+                return solveExpression(endExpression);
+            } else if (search(expression, "+") > 0){
+                start = findStart(expression, "+");
+                end = findEnd(expression, "+");
+
+                if (!expression.substring(0, start).equals("")){
+                    endExpression += expression.substring(0, start);
+                }
+                endExpression += solveExpression(expression.substring(start, end));
+                if (!expression.substring(end, expression.length()).equals("")){
+                    endExpression += expression.substring(end, expression.length());
+                }
+                return solveExpression(endExpression);
+            } else if (search(expression, "-") > 0){
+                start = findStart(expression, "-");
+                end = findEnd(expression, "-");
+                if (!expression.substring(0, start).equals("")){
+                    endExpression += expression.substring(0, start);
+                }
+                endExpression += solveExpression(expression.substring(start, end));
+                if (!expression.substring(end, expression.length()).equals("")){
+                    endExpression += expression.substring(end, expression.length());
+                }
+                return solveExpression(endExpression);
+            } else return 0.0;
         } else return solveOperation(expression);
     }
 
@@ -86,21 +145,23 @@ public class Main{
             expression = expression.substring(1, expression.length() - 1);
         }
         // find the operator
-        if (search(expression, "+")){
+        if (search(expression, "+") > 0){
             ArrayList<Integer> list = new ArrayList<Integer>();
             list = find(expression, "+");
 
             String one = expression.substring(0, list.get(0));
             String two = expression.substring(list.get(0) + 1, expression.length());
+            System.out.println(expression); //ts
+            System.out.println(one + " " + two); //ts
             return Double.parseDouble(one) + Double.parseDouble(two);
-        } else if (search(expression, "-")){
+        } else if (search(expression, "-") > 0){
             ArrayList<Integer> list = new ArrayList<Integer>();
             list = find(expression, "-");
 
             String one = expression.substring(0, list.get(0));
             String two = expression.substring(list.get(0) + 1, expression.length());
             return Double.parseDouble(one) - Double.parseDouble(two);
-        } else if(search(expression, "^")){
+        } else if(search(expression, "^") > 0){
 
             ArrayList<Integer> list = new ArrayList<Integer>();
             list = find(expression, "^");
@@ -108,14 +169,14 @@ public class Main{
             String one = expression.substring(0, list.get(0));
             String two = expression.substring(list.get(0) + 1, expression.length());
             return Math.pow(Double.parseDouble(one), Double.parseDouble(two));
-        }else if (search(expression, "*")){
+        }else if (search(expression, "*") > 0){
             ArrayList<Integer> list = new ArrayList<Integer>();
             list = find(expression, "*");
 
             String one = expression.substring(0, list.get(0));
             String two = expression.substring(list.get(0) + 1, expression.length());
             return Double.parseDouble(one) * Double.parseDouble(two);
-        }else if(search(expression, "/")){
+        }else if(search(expression, "/") > 0){
             ArrayList<Integer> list = new ArrayList<Integer>();
             list = find(expression, "/");
 
@@ -142,43 +203,53 @@ public class Main{
     }
 
     // checks if there is an instance of that operator (character) in the expression 
-    public static boolean search(String expression, String character){
+    public static int search(String expression, String character){
+        int end = 0;
         for (int i = 0; i < expression.length(); i++){
             if (expression.substring(i, i + 1).equals(character)){
-                return true;
+                end += 1;
+                // doesn't count it if the - sign is a negative sign
+                if (character.equals("-") && isSymbol(expression.substring(i - 1, i))){
+                    end -= 1;
+                }
             }
         }
-
-        return false;
+        return end;
     }
 
     // checks if there are multiple operations to solve in the expression
     public static boolean multOperations(String expression){
         int count = 0;
         
-        if (search(expression, "+")){
-            count += 1;
-        }
-        if (search(expression, "-")){
-            count += 1;
-        }
-        if (search(expression, "*")){
-            count += 1;
-        }
-        if (search(expression, "/")){
-            count += 1;
-        }
-        if (search(expression, "^")){
-            count += 1;
-        }
+        count += search(expression, "+");
+        
+        count += search(expression, "-");
+        
+        count += search(expression, "*");
+
+        count += search(expression, "/");
+
+        count += search(expression, "^");
+
         
         if (count == 1){
-            return true;
-        } else return false;
+            return false;
+        } else return true;
     }
 
     public static boolean isOperation(String character){
         String[] operators = {"+", "-", "*", "/", "^"};
+        for (String op : operators){
+            if (op.equals(character)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // used only for checking if a minus sign is a negative sign
+    public static boolean isSymbol(String character){
+        String[] operators = {"+", "-", "*", "/", "^", "("};
         for (String op : operators){
             if (op.equals(character)){
                 return true;
